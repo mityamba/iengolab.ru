b31c351ffa11aa17d340eebb3300aa93cac547eb
mityamba <58911320+mityamba@users.noreply.github.com>
2022-04-25 17:49:56 +0900

    Проверка работы JS - добавление нового персонажа

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,5 @@
+k = 0;
+
 function over(elem, x) {
 
     let id = '#' + elem;
@@ -5,7 +7,7 @@ function over(elem, x) {
     let person_name = document.querySelector(id + '> div.person__name');
     let person_img = document.querySelector(id + '> div.person__img > svg > path');
     if (x == 1) {
-        person.style.transform = 'scale(1.04) translate(0px, -24px)';
+        person.style.transform = 'scale(1.04) translate(0px, -16px)';
         person_name.style.color = '#D64646';
         person_img.style.fill = '#D64646';
     } else {
@@ -16,7 +18,7 @@ function over(elem, x) {
 
 }
 
-let persons = document.querySelectorAll('.person__card');
+var persons = document.querySelectorAll('.person__card');
 console.log(persons.length);
 for (var i = 0; i < persons.length; i++) {
 
@@ -25,19 +27,42 @@ for (var i = 0; i < persons.length; i++) {
         document.getElementById(id).addEventListener("mouseover",
             function() {
                 over(id, 1);
-                setTimeout(function() {
-                    document.getElementById(id).scrollIntoView({
-                        inline: "center", behavior: "smooth"});
-                }, 200);
+                if (k == 0) {
+                    setTimeout(function() {
+                        document.getElementById(id).scrollIntoView({
+                            inline: "center", behavior: "smooth"
+                        });
+                    }, 200);
+                }
+
             }, false);
         document.getElementById(id).addEventListener("mouseout",
             function() {
                 over(id, 0) }, false);
+        document.getElementById(id).addEventListener("click",
+            function() {
+                k = 1;
+                for (var n = 0; n < persons.length; n++) {
+                    if (persons[n].id != id) {
+                        persons[n].style.opacity = 0.1;
+
+                    } else {
+                        persons[n].style.opacity = 1;
+                        setTimeout(function() {
+                            document.getElementById(id).scrollIntoView({
+                                inline: "center", behavior: "smooth"
+                            });
+                        }, 200);
+                    }
+
+                }
+            }, false);
     }
 }
 
 document.getElementById('michael').scrollIntoView({
-    inline: "center", behavior: "smooth"});
+    inline: "center", behavior: "smooth"
+});
 
 circle = document.querySelector('.circle');
 
@@ -51,3 +76,29 @@ document.body.addEventListener("mouseover",
         circle.style.top = circleTop + 'px';
         circle.style.left = circleLeft + 'px';
     }, false);
+
+
+var divPerson = document.querySelector('.person');
+
+let divPersonBlock = document.createElement('div');
+divPersonBlock.className = 'person__card';
+divPersonBlock.id = 'stepan';
+
+let divPersonBlockImg = document.createElement('div');
+divPersonBlockImg.className = 'person__img';
+let link = 'img/person/luba.svg';
+let xhr = new XMLHttpRequest();
+xhr.open('GET', link, false);
+xhr.send();
+let list = xhr.responseText;
+console.log(list);
+divPersonBlockImg.innerHTML = list;
+
+let divPersonBlockName = document.createElement('div');
+divPersonBlockName.className = 'person__name';
+divPersonBlockName.innerHTML = 'Степан';
+
+divPersonBlock.append(divPersonBlockImg);
+divPersonBlock.append(divPersonBlockName);
+
+divPerson.append(divPersonBlock);
